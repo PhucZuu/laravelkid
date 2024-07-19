cbfe3eb760b440687757e0da1be14814be53d7fe
PhucZuu <phucdinhle2004@gmail.com>
2024-07-19 17:01:43 +0700

    fix icon

--- resources/views/client/layouts/partials/css.blade.php
+++ resources/views/client/layouts/partials/css.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
-<link rel="stylesheet" href="fonts/icomoon/style.css">
+<link rel="stylesheet" href="/client/fonts/icomoon/style.css">
 
 <link rel="stylesheet" href="/client/css/bootstrap.min.css">
 <link rel="stylesheet" href="/client/css/magnific-popup.css">
